79467b1e4841b331021484252454cadd27054c0d
Alena <alenad@gmail.com>
2021-04-26 03:38:16 +0000

    Demo code for map with routing

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from flask import session, flash, make_response, redirect
+
+app = Flask(__name__)
+
+# Flask uses a secret key to encrypt cookies used to connect
+# the browser to the session--so if you want to use sessions,
+# you have to have a secret key. If the public learns this
+# value, they can forge session information--so for sites with
+# security concerns, make sure this isn't checked into a
+# public place like GitHub
+app.secret_key = '4534gdghjk5d#$RGR^HDG'
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template('demo.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
